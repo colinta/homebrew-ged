6e42ad894d9aaf45fb27b6bcf615f2bbcaf8650e
Colin T.A. Gray <colinta@gmail.com>
2026-03-09 23:16:59 -0400

    Update to 1.1.0

--- Formula/ged.rb
+++ Formula/ged.rb
@@ -1,21 +1,21 @@
 class Ged < Formula
   desc "Streaming text editor for pipelines — modern sed alternative"
   homepage "https://github.com/colinta/ged"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/colinta/ged/releases/download/1.0.0/ged-darwin-arm64.tar.gz"
-      sha256 "82ee254cdfe4f0d9e928d71f0e760be6af9aacef944bea2596735899ad42e035"
+      url "https://github.com/colinta/ged/releases/download/1.1.0/ged-darwin-arm64.tar.gz"
+      sha256 "63306dd9e025f2c80c2473db4d47bc44daf1414d5ca36e98c861b59b0c22e3cb"
 
       def install
         bin.install "ged-darwin-arm64" => "ged"
         man1.install "ged.1"
       end
     else
-      url "https://github.com/colinta/ged/releases/download/1.0.0/ged-darwin-amd64.tar.gz"
-      sha256 "d2e89761a3e07cedffe5db451976dc6917a83b62ef672a43777a21b01bad8dd6"
+      url "https://github.com/colinta/ged/releases/download/1.1.0/ged-darwin-amd64.tar.gz"
+      sha256 "dfcce7201c2a5a8e98ee5e6f6567364a6220e106e21c5d554c97981382059ef8"
 
       def install
         bin.install "ged-darwin-amd64" => "ged"
@@ -26,16 +26,16 @@ class Ged < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/colinta/ged/releases/download/1.0.0/ged-linux-arm64.tar.gz"
-      sha256 "a1067fb096e2ddc18d31a807bce2c56e3752b91ed2796b56391486223c21e23c"
+      url "https://github.com/colinta/ged/releases/download/1.1.0/ged-linux-arm64.tar.gz"
+      sha256 "5696a5f4e22d7f46d72fcc1a767c46409740141334a6361aa42e0f01a89a1822"
 
       def install
         bin.install "ged-linux-arm64" => "ged"
         man1.install "ged.1"
       end
     else
-      url "https://github.com/colinta/ged/releases/download/1.0.0/ged-linux-amd64.tar.gz"
-      sha256 "943a566f65039e275b695805c91399ea5fd072f32cc67dca422598fe2617a99c"
+      url "https://github.com/colinta/ged/releases/download/1.1.0/ged-linux-amd64.tar.gz"
+      sha256 "898286addec513a9f2f18cef8b5942e41dd740b163f54729460d20886b4d5723"
 
       def install
         bin.install "ged-linux-amd64" => "ged"
