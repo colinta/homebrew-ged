3344270776a9410041e7cbf0f7885fd159f4525a
Colin T.A. Gray <colinta@gmail.com>
2026-03-27 14:55:37 -0400

    ged 1.5.2

--- Formula/ged.rb
+++ Formula/ged.rb
@@ -1,21 +1,21 @@
 class Ged < Formula
   desc "Streaming text editor for pipelines — modern sed alternative"
   homepage "https://github.com/colinta/ged"
-  version "1.4.2"
+  version "1.5.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/colinta/ged/releases/download/1.4.2/ged-darwin-arm64.tar.gz"
-      sha256 "f214862ad323878347281d136c5328c56e2d1b2edd4fd377877d917704f808b1"
+      url "https://github.com/colinta/ged/releases/download/1.5.2/ged-darwin-arm64.tar.gz"
+      sha256 "6ba695695a93f08898fbe376abb11d8ccd718c3abce1ffaf8f09ad96b6ce587a"
 
       def install
         bin.install "ged-darwin-arm64" => "ged"
         man1.install "ged.1"
       end
     else
-      url "https://github.com/colinta/ged/releases/download/1.4.2/ged-darwin-amd64.tar.gz"
-      sha256 "ec18814c2fcbee24174440ac9b3814e413e8c7147045af950fe55cbd3b6715f3"
+      url "https://github.com/colinta/ged/releases/download/1.5.2/ged-darwin-amd64.tar.gz"
+      sha256 "059803c21baa3aba27f68172284284df22e4d5e29766dac46fb20af72bfd36a9"
 
       def install
         bin.install "ged-darwin-amd64" => "ged"
@@ -26,16 +26,16 @@ class Ged < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/colinta/ged/releases/download/1.4.2/ged-linux-arm64.tar.gz"
-      sha256 "5e38d627b80bbc150a55c8910a66570f0cb58ac8f2b110f276c486128deba667"
+      url "https://github.com/colinta/ged/releases/download/1.5.2/ged-linux-arm64.tar.gz"
+      sha256 "d06e6c3c3d45fc866a1d774acc0c726e9013d64b7acd655894319b21bde75d37"
 
       def install
         bin.install "ged-linux-arm64" => "ged"
         man1.install "ged.1"
       end
     else
-      url "https://github.com/colinta/ged/releases/download/1.4.2/ged-linux-amd64.tar.gz"
-      sha256 "5ee6f01596baff4a5c4115176358d8d5be0718772aa226e8651426c0b8b56c70"
+      url "https://github.com/colinta/ged/releases/download/1.5.2/ged-linux-amd64.tar.gz"
+      sha256 "09a7bf76650009a7f88c0fbd742744d8036d0b4a40f0f74bd3cdafc43f45db67"
 
       def install
         bin.install "ged-linux-amd64" => "ged"
